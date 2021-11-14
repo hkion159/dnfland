dac7dbd686f0321c35aa5470d3a75d5dfd1d77a0
JSWCOMPUTER\JSW <hkion159@naver.com>
2021-11-14 23:52:12 +0900

    add king

--- components/box/mainbox.js
+++ components/box/mainbox.js
@@ -49,7 +49,7 @@ function MainBox({ children }) {
           </div>
           {!session && (
             <div className="py-1 px-3" style={{ borderTop: '1px solid #e0e0e0' }}>
-              로그인을 해보세요!
+              로그인해보세요!
             </div>
           )}
           {session && characters?.length === 0 && (
@@ -57,6 +57,16 @@ function MainBox({ children }) {
               캐릭터를 검색하고
               <br />
               모험단에 등록하세요!
+            </div>
+          )}
+          {session && king && (
+            <div className="py-1 px-3" style={{ borderTop: '1px solid #e0e0e0' }}>
+              <Link href={`/character?characterid=${king?.characterId}&serverid=${king?.serverId}`}>
+                <a className="link-black">
+                  <i className="align-baseline bi bi-play"></i>
+                  <b>{king?.name}</b>
+                </a>
+              </Link>
             </div>
           )}
           <div className="overflow-scroll">
